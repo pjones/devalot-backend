274302194a1584555284d856289189e2f9905631
Peter Jones <pjones@devalot.com>
2021-06-03 13:12:30 -0700

    Updates for NixOS 21.05

--- default.nix
+++ default.nix
@@ -1,21 +1,10 @@
 { sources ? import ./nix/sources.nix
 , pkgs ? import sources.nixpkgs { }
 , nix-hs ? import sources.nix-hs { inherit pkgs; }
-, ghcide ? sources.ghcide-nix
-, ormolu ? sources.ormolu
 , ghc ? "default"
 }:
 
 nix-hs {
   cabal = ./devalot-backend.cabal;
   compiler = ghc;
-
-  overrides = lib: self: super: with lib; {
-    ghcide = import ghcide {};
-
-    ormolu = (import ormolu {
-      inherit (lib) pkgs;
-      ormoluCompiler = lib.compilerName;
-    }).ormolu;
-  };
 }
--- src/Devalot/Backend/App.hs
+++ src/Devalot/Backend/App.hs
@@ -1,14 +1,8 @@
-{-# LANGUAGE DataKinds     #-}
-{-# LANGUAGE TypeOperators #-}
-
---------------------------------------------------------------------------------
 module Devalot.Backend.App (app) where
 
---------------------------------------------------------------------------------
 import Devalot.Backend.Feedback
 import Network.Wai (Application)
 import Servant.Server (serve)
 
---------------------------------------------------------------------------------
 app :: Application
 app = serve feedbackAPI feedbackServer
